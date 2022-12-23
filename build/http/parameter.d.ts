ae16e9740b74001bcf08dcc1ec9a6b5b84435d17
thieren <22128659+thieren@users.noreply.github.com>
2022-12-23 11:31:08 +0100

    remove build dir

--- build/http/parameter.d.ts
+++
@@ -1,5 +0,0 @@
-import { Logger } from "ts-log";
-export declare class ParameterHelper {
-    static readValue(type: number, value: string, log: Logger): string;
-    static writeValue(type: number, value: string): string;
-}
